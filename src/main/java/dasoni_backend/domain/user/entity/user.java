2f04233498c1b226a58209b145c59412fa16507d
youngroky <youngrok886@gmail.com>
2025-10-24 17:27:18 +0900

    feat: user entity

--- src/main/java/dasoni_backend/domain/user/entity/user.java
+++ src/main/java/dasoni_backend/domain/user/entity/user.java
@@ -5,9 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Lob;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.Size;
 
 import java.time.LocalDateTime;
 
