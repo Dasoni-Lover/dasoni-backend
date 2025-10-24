1098659491beeb6b1d5b41894d56e496ecd622f7
youngroky <youngrok886@gmail.com>
2025-10-24 17:26:38 +0900

    feat: gradle 설정추가

--- src/main/java/dasoni_backend/domain/hall/entity/hall.java
+++ src/main/java/dasoni_backend/domain/hall/entity/hall.java
@@ -1,4 +1,18 @@
 package dasoni_backend.domain.hall.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+
+@Entity
+@Getter
+@Table(name = "halls")
 public class hall {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
 }
--- src/main/java/dasoni_backend/domain/user/entity/user.java
+++ src/main/java/dasoni_backend/domain/user/entity/user.java
@@ -1,4 +1,39 @@
 package dasoni_backend.domain.user.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Size;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
 public class user {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 50, nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Boolean gender;
+    // true: 여자, false : 남자
+
+    @Column(nullable = false)
+    private LocalDateTime birthday;
+
+    @Column(name = "log_id", length = 100, nullable = false, unique = true)
+    private String logId;
+
+    @Column(length = 100, nullable = false)
+    private String password;
+
+    @Column(name = "my_profile",columnDefinition = "TEXT")
+    private String myProfile;
 }
