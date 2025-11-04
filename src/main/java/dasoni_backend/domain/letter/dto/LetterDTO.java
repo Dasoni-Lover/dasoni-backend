e6fee9b2af0e8599900d05f8e9b57a7f11fd6f3b
ududjjang <chocynthia@naver.com>
2025-11-05 01:54:43 +0900

    refactor: 엔티티 구조 수정

--- src/main/java/dasoni_backend/domain/letter/dto/LetterDTO.java
+++ src/main/java/dasoni_backend/domain/letter/dto/LetterDTO.java
@@ -15,38 +15,33 @@ public class LetterDTO {
     // 1. 보낸 편지함 조회
     @Getter
     @Builder
-    @AllArgsConstructor
-    @NoArgsConstructor
     public static class SentLetterListResponseDTO {
 
         private int count;
 
         private List<SentLetterResponseDTO> letters;
-
-        @Getter
-        @Builder
-        @AllArgsConstructor
-        @NoArgsConstructor
-        public static class SentLetterResponseDTO {
-
-            private Long letterId;
-
-            // completedAt, 최신순 조회
-            // 2번 필드랑 통일 추후 통일(date -> completedAt)
-            @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy.MM.dd")
-            private LocalDateTime date;
-
-            private String toName;
-
-            private String excerpt;
-        }
     }
+
+    @Getter
+    @Builder
+    public static class SentLetterResponseDTO {
+
+        private Long letterId;
+
+        // completedAt, 최신순 조회
+        // 2번 필드랑 통일 추후 통일(date -> completedAt)
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy.MM.dd")
+        private LocalDateTime date;
+
+        private String toName;
+
+        private String excerpt;
+    }
+    
 
     // 2. 보낸 편지 확인(내용)
     @Getter
     @Builder
-    @AllArgsConstructor
-    @NoArgsConstructor
     public static class SentLetterDetailResponseDTO {
 
         private String toName;
@@ -62,22 +57,17 @@ public class LetterDTO {
     // 3. 보낸 편지함 달력 조회
     @Getter
     @Builder
-    @AllArgsConstructor
-    @NoArgsConstructor
     public static class SentLetterCalenderListResponseDTO {
 
         private List<SentLetterCalenderResponseDTO> days;
+    }
+    @Getter
+    @Builder
+    public static class SentLetterCalenderResponseDTO {
 
-        @Getter
-        @Builder
-        @AllArgsConstructor
-        @NoArgsConstructor
-        public static class SentLetterCalenderResponseDTO {
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy.MM.dd")
+        private LocalDateTime date;
 
-            @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy.MM.dd")
-            private LocalDateTime date;
-
-            private Long letterId;
-        }
+        private Long letterId;
     }
 }
