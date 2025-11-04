069c6bd45c7b283359592664a6aba1c599810289
ududjjang <chocynthia@naver.com>
2025-11-05 02:00:52 +0900

    refactor: ResponseEntity로 감싸기

--- src/main/java/dasoni_backend/domain/letter/controller/LetterController.java
+++ src/main/java/dasoni_backend/domain/letter/controller/LetterController.java
@@ -5,6 +5,7 @@ import dasoni_backend.domain.letter.dto.LetterDTO.SentLetterDetailResponseDTO;
 import dasoni_backend.domain.letter.dto.LetterDTO.SentLetterListResponseDTO;
 import dasoni_backend.domain.letter.service.LetterService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,18 +20,18 @@ public class LetterController {
     private final LetterService letterService;
 
     @GetMapping("{hall_id}/letters/list")
-    public SentLetterListResponseDTO getSentLetterList(@PathVariable("hall_id") Long hallId, @RequestParam(name = "user_id") Long userId) {
-        return letterService.getSentLetterList(hallId, userId);
+    public ResponseEntity<SentLetterListResponseDTO> getSentLetterList(@PathVariable("hall_id") Long hallId, @RequestParam(name = "user_id") Long userId) {
+        return ResponseEntity.ok(letterService.getSentLetterList(hallId, userId));
     }
 
     @GetMapping("{hall_id}/letters/{letter_id}")
-    public SentLetterDetailResponseDTO getSentLetterDetail(@PathVariable("hall_id") Long hallId, @PathVariable("letter_id") Long letterId) {
-        return letterService.getSentLetterDetail(hallId, letterId);
+    public ResponseEntity<SentLetterDetailResponseDTO> getSentLetterDetail(@PathVariable("hall_id") Long hallId, @PathVariable("letter_id") Long letterId) {
+        return ResponseEntity.ok(letterService.getSentLetterDetail(hallId, letterId));
     }
 
     @GetMapping("{hall_id}/letters/calendar")
-    public SentLetterCalenderListResponseDTO getSentLetterCalenderList(@PathVariable("hall_id") Long hallId, @RequestParam("user_id") Long userId
+    public ResponseEntity<SentLetterCalenderListResponseDTO> getSentLetterCalenderList(@PathVariable("hall_id") Long hallId, @RequestParam("user_id") Long userId
             , @RequestParam int year, @RequestParam int month) {
-        return letterService.getSentLetterCalenderList(hallId, userId, year, month);
+        return ResponseEntity.ok(letterService.getSentLetterCalenderList(hallId, userId, year, month));
     }
 }
